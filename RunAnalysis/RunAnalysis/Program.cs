88a8b1e24356899d5c1b0fc76e70b280ee4c1b81
Nitin Agarwal <nagarwal@osisoft.com>
2016-04-27 14:24:42 -0700

    Solution setup

---
+++ RunAnalysis/RunAnalysis/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RunAnalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
